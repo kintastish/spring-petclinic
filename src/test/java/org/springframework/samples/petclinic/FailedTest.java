ad0eb710172dba816906d22eb283d429c095c376
Iurii_Nemnonov <Iurii_Nemnonov@epam.com>
2018-10-26 14:14:02 +0300

    Test modified

--- src/test/java/org/springframework/samples/petclinic/FailedTest.java
+++ src/test/java/org/springframework/samples/petclinic/FailedTest.java
@@ -1,13 +1,11 @@
 package org.springframework.samples.petclinic;
 
-import static org.assertj.core.api.Assertions.assertTrue;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 public class FailedTest {
     @Test
     public void alwaysFail() {
-        assertTrue(false);
+        throw new RuntimeException("Fail");
     }
 }
